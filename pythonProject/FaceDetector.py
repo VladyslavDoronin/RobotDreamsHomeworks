a897cfab1008c6856f54d3f2555cefc90ecc87e2
VladyslavDoronin <45238790+VladyslavDoronin@users.noreply.github.com>
2024-06-21 10:32:41 +0300

    dlib install

--- pythonProject/FaceDetector.py
+++ pythonProject/FaceDetector.py
@@ -47,6 +47,81 @@ plt.subplot(122), plt.imshow(faces_img[0])
 plt.show()
 
 
+# Face Detection via dlib
+# Dlib is a general purpose cross-platform software library that contains many useful tools. In particular, it includes a trained DNN for face detection.
+import dlib
+
+casc_path = '/home/user/Documents/GitHub/RobotDreamsHomeworks/pythonProject/venv/lib/python3.10/site-packages/cv2/data/haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(casc_path)
+img = cv2.imread('data/myPhoto.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+plt.imshow(img)
+plt.show()
+# Let's load the detector
+detector = dlib.get_frontal_face_detector()
+# Detect faces, see http://dlib.net/face_detector.py.html
+# 1 --> upsampling factor
+rects = detector(gray, 1)
+
+print('Number of detected faces:', len(rects))
+print(rects)
+print(rects[0].left)
+
+
+def rect_to_bb(rect):
+    # Dlib rect --> OpenCV rect
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+
+    return (x, y, w, h)
+
+
+# Draw rectangle around each face
+result_dlib = np.copy(img)
+faces_dlib_img = []
+for rect in rects:
+    # Draw rectangle around the face
+    x, y, w, h = rect_to_bb(rect)
+    print(x, y, w, h)
+    cv2.rectangle(result_dlib, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    faces_dlib_img.append(img[y:y + h, x:x + w, :])
+
+plt.subplot(121), plt.imshow(result), plt.title('Viola-Jones')
+plt.subplot(122), plt.imshow(result_dlib), plt.title('dlib')
+plt.show()
+
+
+
+# Facial Landmarks
+# Facial landmarks can be considred as keypoints that define the contours of a face, eyes, nose and mouth. There are many models for facial landmarking, one of the most popular is the 68-point model (https://link.springer.com/article/10.1007/s11554-021-01107-w)
+
+# Load the predictor (you need to download the predictor from an available source)
+predictor = dlib.shape_predictor('data/shape_predictor_68_face_landmarks.dat')
+
+
+face = faces_dlib_img[1]
+rows, cols, _ = face.shape
+rect = dlib.rectangle(0, 0, cols, rows)
+shape = predictor(cv2.cvtColor(face, cv2.COLOR_RGB2GRAY), rect)
+def plot_landmarks(vec):
+    plt.plot(vec[0:17,0], vec[0:17,1], 'g.-')       # Contour
+    plt.plot(vec[48:68,0], vec[48:68,1], 'g.-')     # Mouth
+    plt.plot(vec[17:22,0], vec[17:22,1], 'g.-')     # Right eyebrow
+    plt.plot(vec[22:27,0], vec[22:27,1], 'g.-')     # Left eyebrow
+    plt.plot(np.concatenate((vec[36:42,0], vec[36:37,0])), np.concatenate((vec[36:42,1], vec[36:37,1])), 'g.-')     # Left eye
+    plt.plot(np.concatenate((vec[42:48,0], vec[42:43,0])), np.concatenate((vec[42:48,1], vec[42:43,1])), 'g.-')     # Right eye
+    plt.plot(vec[27:36,0], vec[27:36,1], 'g.-')     # Nose
+# Convert landmarks to ndarray for better manipulation
+kps = list(map(lambda p: (p.x, p.y), shape.parts()))
+landmarks = np.array(kps)
+print(kps)
+plt.imshow(face)
+plt.show()
+plot_landmarks(landmarks)
+plt.show()
 # Face Alignment
 # Face alignment is an important pre-processing step for face identification. The goal is to transform the detected faces as it was taken in frontal view. In general, only affine transforms are considered so the facial proportions are maintained.
 
@@ -56,6 +131,7 @@ tpl_min, tpl_max = np.min(face_template, axis=0), np.max(face_template, axis=0)
 face_template = (face_template - tpl_min) / (tpl_max - tpl_min)
 
 plt.plot(face_template[:, 0], -face_template[:, 1], 'o')
+plt.show()
 
 inner_triangle = [39, 42, 57]
 plt.plot(face_template[inner_triangle, 0], -face_template[inner_triangle, 1], 'rs')
@@ -82,3 +158,4 @@ aligned = cv2.warpAffine(out, H, (out_size + margin, out_size + margin))
 
 plt.subplot(121), plt.imshow(face), plt.title('Detected')
 plt.subplot(122), plt.imshow(aligned), plt.title('Aligned')
+plt.show()
