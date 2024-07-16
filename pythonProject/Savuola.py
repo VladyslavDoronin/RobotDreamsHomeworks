564cfa759ed4831da37d9d199bbfbb879ebc2d79
VladyslavDoronin <45238790+VladyslavDoronin@users.noreply.github.com>
2024-07-16 11:40:31 +0300

    Create Savuola.py

---
+++ pythonProject/Savuola.py
@@ -0,0 +1,48 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import cv2
+
+
+from skimage.data import page
+from skimage.filters import threshold_otsu, threshold_niblack, threshold_sauvola
+
+
+matplotlib.rcParams['font.size'] = 9
+
+image = cv2.imread('data/document.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(image, cmap='gray')
+plt.show()
+# First, let's have a look at the his
+# image = page()
+binary_global = image > threshold_otsu(image)
+
+window_size = 25
+thresh_niblack = threshold_niblack(image, window_size=window_size, k=0.8)
+thresh_sauvola = threshold_sauvola(image, window_size=window_size)
+
+binary_niblack = image > thresh_niblack
+binary_sauvola = image > thresh_sauvola
+
+plt.figure(figsize=(8, 7))
+plt.subplot(2, 2, 1)
+plt.imshow(image, cmap=plt.cm.gray)
+plt.title('Original')
+plt.axis('off')
+
+plt.subplot(2, 2, 2)
+plt.title('Global Threshold')
+plt.imshow(binary_global, cmap=plt.cm.gray)
+plt.axis('off')
+
+plt.subplot(2, 2, 3)
+plt.imshow(binary_niblack, cmap=plt.cm.gray)
+plt.title('Niblack Threshold')
+plt.axis('off')
+
+plt.subplot(2, 2, 4)
+plt.imshow(binary_sauvola, cmap=plt.cm.gray)
+plt.title('Sauvola Threshold')
+plt.axis('off')
+
+plt.show()
